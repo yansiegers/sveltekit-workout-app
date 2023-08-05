8ee33f938a0e3b66eae5d4f517e4d1df2c9440e7
Yan Siegers <hello@yansiegers.nl>
2023-08-05 19:13:19 +0200

    Make header customizable

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,9 @@
+<script lang="ts">
+	export let heading = 'Workout app';
+	export let subHeading = 'Friday 4th of August';
+</script>
+
 <div class="mb-8">
-	<h1 class="text-3xl font-bold">Workout app</h1>
-	<div class="text-xl" role="doc-subtitle">Friday 4th of August</div>
+	<h1 class="text-3xl font-bold">{heading}</h1>
+	<div class="text-xl" role="doc-subtitle">{subHeading}</div>
 </div>
