546f15d550b15d4cd5349003c2056103732460df
Yan Siegers <hello@yansiegers.nl>
2023-08-05 19:16:22 +0200

    Add ExercisesList component

---
+++ src/components/ExercisesList.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div class="mb-8">
+	<h2 class="mb-2 font-bold">{title}</h2>
+	<div class="border-2 border-black p-4 mb-4 text-2xl">Full body</div>
+	<div class="border-2 border-black p-4 mb-4 text-2xl">Chest & back</div>
+	<div class="border-2 border-black p-4 mb-4 text-2xl">Arms & shoulders</div>
+</div>
--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,4 +1,5 @@
 <script>
+	import ExercisesList from '../components/ExercisesList.svelte';
 	import Header from '../components/Header.svelte';
 </script>
 
@@ -9,9 +10,4 @@
 	<div class="border-2 border-black p-4 mb-4 text-2xl">Chest & back</div>
 </div>
 
-<div class="mb-8">
-	<h2 class="mb-2 font-bold">Favourites</h2>
-	<div class="border-2 border-black p-4 mb-4 text-2xl">Full body</div>
-	<div class="border-2 border-black p-4 mb-4 text-2xl">Chest & back</div>
-	<div class="border-2 border-black p-4 mb-4 text-2xl">Arms & shoulders</div>
-</div>
+<ExercisesList title={'Favourites'} />
