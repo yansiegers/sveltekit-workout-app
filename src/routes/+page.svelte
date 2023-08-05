e83c6367e431b98498756f0caf4b459288cbc2ff
Yan Siegers <hello@yansiegers.nl>
2023-08-05 19:49:42 +0200

    Add navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,18 @@
 	import Home from '../components/Home.svelte';
 </script>
 
-<Home />
+<div class="h-screen relative">
+	<main class="p-8 mb-16">
+		<Home />
+	</main>
+	<div
+		class="border-t-2 border-black text-center flex justify-evenly fixed bottom-0 w-screen bg-white"
+	>
+		<div class="p-4 w-16 h-16">W</div>
+		<div class="p-4 w-16 h-16">S</div>
+		<div class="p-4 w-16 h-16 border-2 border-black absolute bottom-4 bg-white">+</div>
+		<div class="p-4 w-16 h-16" />
+		<div class="p-4 w-16 h-16">H</div>
+		<div class="p-4 w-16 h-16">A</div>
+	</div>
+</div>
