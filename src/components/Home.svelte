72995311740494ed6e7d8fddeb385f3f20657466
Yan Siegers <hello@yansiegers.nl>
2023-08-05 18:16:47 +0200

    Add structured content

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,1 +1,16 @@
-<h1>Home</h1>
+<div class="header">
+	<h1>Workout app</h1>
+	<div role="doc-subtitle">Subheading</div>
+</div>
+
+<div>
+	<h2>Scheduled today</h2>
+	<div>Chest & back</div>
+</div>
+
+<div>
+	<h2>Favourites</h2>
+	<div>Full body</div>
+	<div>Chest & back</div>
+	<div>Arms & shoulders</div>
+</div>
