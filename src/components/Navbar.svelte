1545251799ef907d296298c53e86ee226f1873c8
Yan Siegers <hello@yansiegers.nl>
2023-08-08 20:43:26 +0200

    Add links to navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -6,18 +6,18 @@
 	<div class="h-16 w-16 p-4">
 		<Icon name="dumbbell" size="lg" />
 	</div>
-	<div class="h-16 w-16 p-4">
+	<a class="h-16 w-16 p-4" href="/schedule">
 		<Icon name="calendar" size="lg" />
-	</div>
+	</a>
 	<div class="absolute bottom-4 h-16 w-16 border-2 border-black bg-white p-4">
 		<Icon name="plus" size="lg" />
 	</div>
 	<div class="h-16 w-16">
 		<!-- placeholder -->
 	</div>
-	<div class="h-16 w-16 p-4">
+	<a class="h-16 w-16 p-4" href="/">
 		<Icon name="home" size="lg" />
-	</div>
+	</a>
 	<div class="h-16 w-16 p-4">
 		<Icon name="user" size="lg" />
 	</div>
