7b885c391172dd62acffe10873feb0a89a951d15
Yan Siegers <hello@yansiegers.nl>
2023-08-08 20:26:18 +0200

    Add Icon component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let name: string;
+	export let size: '2xs' | 'xs' | 'sm' | '' | 'lg' | 'xl' | '2xl' = '';
+	let faSize = size ? 'fa-' + size : '';
+</script>
+
+<i class="fa-solid fa-{name} {faSize}" />
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,10 +1,24 @@
+<script>
+	import Icon from '../components/Icon.svelte';
+</script>
+
 <div class="flex justify-evenly border-t-2 border-black bg-white text-center">
-	<div class="h-16 w-16 p-4"><i class="fa-solid fa-dumbbell fa-lg" /></div>
-	<div class="h-16 w-16 p-4"><i class="fa-solid fa-calendar fa-lg" /></div>
-	<div class="absolute bottom-4 h-16 w-16 border-2 border-black bg-white p-4">
-		<i class="fa-solid fa-plus fa-lg" />
+	<div class="h-16 w-16 p-4">
+		<Icon name="dumbbell" size="lg" />
 	</div>
-	<div class="fa-lg h-16 w-16 p-4" />
-	<div class="h-16 w-16 p-4"><i class="fa-solid fa-home fa-lg" /></div>
-	<div class="h-16 w-16 p-4"><i class="fa-solid fa-user fa-lg" /></div>
+	<div class="h-16 w-16 p-4">
+		<Icon name="calendar" size="lg" />
+	</div>
+	<div class="absolute bottom-4 h-16 w-16 border-2 border-black bg-white p-4">
+		<Icon name="plus" size="lg" />
+	</div>
+	<div class="h-16 w-16">
+		<!-- placeholder -->
+	</div>
+	<div class="h-16 w-16 p-4">
+		<Icon name="home" size="lg" />
+	</div>
+	<div class="h-16 w-16 p-4">
+		<Icon name="user" size="lg" />
+	</div>
 </div>
