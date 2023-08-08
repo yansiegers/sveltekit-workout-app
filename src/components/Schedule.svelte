3ae5b0079c4224a3e7441c821a3fd730e0ccf2c9
Yan Siegers <hello@yansiegers.nl>
2023-08-08 20:42:17 +0200

    Add schedule page and component

---
+++ src/components/Schedule.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from './Header.svelte';
+	import WorkoutRoutinesList from './WorkoutRoutinesList.svelte';
+
+	let mondayWorkoutRoutines = [{ name: 'Legs' }];
+	let wednesdayWorkoutRoutine = [{ name: 'Arms & shoulders' }];
+	let fridayWorkoutRoutine = [{ name: 'Chest & back' }];
+
+	let otherWorkoutRoutines = [{ name: 'Full body' }];
+</script>
+
+<Header heading="Schedule" />
+
+<WorkoutRoutinesList title={'Monday'} workoutRoutines={mondayWorkoutRoutines} />
+<WorkoutRoutinesList title={'Wednesday'} workoutRoutines={wednesdayWorkoutRoutine} />
+<WorkoutRoutinesList title={'Friday'} workoutRoutines={fridayWorkoutRoutine} />
+<WorkoutRoutinesList title={'Other workouts'} workoutRoutines={otherWorkoutRoutines} />
---
+++ src/routes/schedule/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Schedule from '../../components/Schedule.svelte';
+	import Navbar from '../../components/Navbar.svelte';
+</script>
+
+<div class="flex h-screen flex-col">
+	<main class="h-full overflow-y-scroll p-8">
+		<Schedule />
+	</main>
+	<Navbar />
+</div>
