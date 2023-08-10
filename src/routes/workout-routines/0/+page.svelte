6797fd412b6e8471c0a43fa84494f92b4d6f12a7
Yan Siegers <hello@yansiegers.nl>
2023-08-10 18:19:37 +0200

    Add table

--- src/routes/workout-routines/0/+page.svelte
+++ src/routes/workout-routines/0/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Header from '../../../components/Header.svelte';
+	import Icon from '../../../components/Icon.svelte';
 
 	let workoutRoutine = { id: 0, name: 'Chest & back' };
 	let subHeading = 'Overview Friday 4th of August';
@@ -8,5 +9,17 @@
 <div class="flex h-screen flex-col">
 	<main class="h-full overflow-y-scroll p-8">
 		<Header heading={workoutRoutine.name} {subHeading} />
+
+		<div>
+			<div class="mb-2 font-bold">Workout</div>
+			<table class="w-full">
+				<tr>
+					<td><Icon name="grip-vertical" /></td>
+					<td>4x 10</td>
+					<td>Shoulder Press</td>
+					<td>24,1</td>
+				</tr>
+			</table>
+		</div>
 	</main>
 </div>
