1ceb17d61900b297ddd6416a46b381e4276bf490
Yan Siegers <hello@yansiegers.nl>
2023-08-10 22:11:49 +0200

    Add more routines to home

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -8,9 +8,19 @@
 		{ id: 0, name: 'Chest & back' },
 		{ id: 0, name: 'Arms & shoulders' }
 	];
+	let otherWorkoutRoutines = [
+		{ id: 0, name: 'Arms' },
+		{ id: 0, name: 'Chest' },
+		{ id: 0, name: 'Back' },
+		{ id: 0, name: 'Shoulders' },
+		{ id: 0, name: 'Core' },
+		{ id: 0, name: 'Back' },
+		{ id: 0, name: 'Legs' }
+	];
 </script>
 
 <Header />
 
 <WorkoutRoutinesList title={'Scheduled today'} workoutRoutines={scheduledWorkoutRoutines} />
 <WorkoutRoutinesList title={'Favourites'} workoutRoutines={favoriteWorkoutRoutines} />
+<WorkoutRoutinesList title={'Other workouts'} workoutRoutines={otherWorkoutRoutines} />
