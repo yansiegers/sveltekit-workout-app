c50cd63f55c82299287087b3a0623db191240149
Yan Siegers <hello@yansiegers.nl>
2023-08-10 08:03:41 +0200

    Add routine overview page

---
+++ src/routes/workout-routines/0/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from '../../../components/Header.svelte';
+
+	let workoutRoutine = { id: 0, name: 'Chest & back' };
+	let subHeading = 'Overview Friday 4th of August';
+</script>
+
+<div class="flex h-screen flex-col">
+	<main class="h-full overflow-y-scroll p-8">
+		<Header heading={workoutRoutine.name} {subHeading} />
+	</main>
+</div>
