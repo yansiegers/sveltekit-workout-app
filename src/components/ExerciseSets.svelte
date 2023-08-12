f401f5aa81e1cd205dff988567667f7be3442a6e
Yan Siegers <hello@yansiegers.nl>
2023-08-12 22:36:55 +0200

    Add resting state

--- src/components/ExerciseSets.svelte
+++ src/components/ExerciseSets.svelte
@@ -6,6 +6,7 @@
 
 	let finishedSets = [24.1, 24.1];
 	let setsCount = finishedSets.length > exercise.sets ? finishedSets.length : exercise.sets;
+	let resting = false;
 
 	interface WorkoutExercise {
 		type: 'workoutExercise';
@@ -16,20 +17,24 @@
 	}
 </script>
 
-<p class="mb-3 text-center">{exercise.sets}x {exercise.reps} repetitions</p>
-<div class="flex flex-wrap justify-center gap-4">
-	{#each Array(setsCount) as _, i}
-		<SetButton>
-			{#if finishedSets.length > i}
-				<span><Icon name="check" size="2xl" /></span>
-				<span>{finishedSets[i].toLocaleString()} kg</span>
-			{:else if finishedSets.length <= i}
-				<span class="text-2xl">{i + 1}</span>
-			{/if}
-		</SetButton>
-	{/each}
+{#if resting}
+	resting
+{:else}
+	<p class="mb-3 text-center">{exercise.sets}x {exercise.reps} repetitions</p>
+	<div class="flex flex-wrap justify-center gap-4">
+		{#each Array(setsCount) as _, i}
+			<SetButton>
+				{#if finishedSets.length > i}
+					<span><Icon name="check" size="2xl" /></span>
+					<span>{finishedSets[i].toLocaleString()} kg</span>
+				{:else if finishedSets.length <= i}
+					<span class="text-2xl">{i + 1}</span>
+				{/if}
+			</SetButton>
+		{/each}
 
-	<SetButton>
-		<span><Icon name="plus" size="2xl" /></span>
-	</SetButton>
-</div>
+		<SetButton>
+			<span><Icon name="plus" size="2xl" /></span>
+		</SetButton>
+	</div>
+{/if}
