927a8bfa2372587dcdc48b4a8c187ec95a2296e0
Yan Siegers <hello@yansiegers.nl>
2023-08-12 19:53:12 +0200

    Add SlotButton element

--- src/components/ExerciseSets.svelte
+++ src/components/ExerciseSets.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Icon from './Icon.svelte';
+	import SetButton from './SetButton.svelte';
 
 	export let exercise: WorkoutExercise;
 
@@ -18,17 +19,17 @@
 <p class="mb-3 text-center">{exercise.sets}x {exercise.reps} repetitions</p>
 <div class="flex flex-wrap justify-center gap-4">
 	{#each Array(setsCount) as _, i}
-		<div class="flex h-20 w-20 flex-col justify-evenly border-2 border-black text-center">
+		<SetButton>
 			{#if finishedSets.length > i}
 				<span><Icon name="check" size="2xl" /></span>
 				<span>{finishedSets[i].toLocaleString()} kg</span>
 			{:else if finishedSets.length <= i}
 				<span class="text-2xl">{i + 1}</span>
 			{/if}
-		</div>
+		</SetButton>
 	{/each}
 
-	<div class="flex h-20 w-20 flex-col justify-evenly border-2 border-black text-center">
+	<SetButton>
 		<span><Icon name="plus" size="2xl" /></span>
-	</div>
+	</SetButton>
 </div>
---
+++ src/components/SetButton.svelte
@@ -0,0 +1,3 @@
+<div class="flex h-20 w-20 flex-col justify-evenly border-2 border-black text-center">
+	<slot />
+</div>
