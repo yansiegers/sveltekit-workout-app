6c0b2130f271d7e4da9d012d29d33a3072f0d2a2
Yan Siegers <hello@yansiegers.nl>
2023-08-12 11:46:03 +0200

    Add set indicators

--- src/routes/workout-routines/0/exercises/+page.svelte
+++ src/routes/workout-routines/0/exercises/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import FloatingButton from '../../../../components/FloatingButton.svelte';
 	import Header from '../../../../components/Header.svelte';
+	import Icon from '../../../../components/Icon.svelte';
 
 	let exercise = { id: 0, name: 'Shoulder Press' };
 	let subHeading = 'Up next: Pec Fly';
@@ -11,5 +12,25 @@
 		<Header heading={exercise.name} {subHeading} exit={true} href="/" />
 
 		<FloatingButton text="Next exercise!" href="?" />
+
+		<div class="flex flex-wrap justify-center gap-4">
+			<div class="flex h-20 w-20 flex-col justify-evenly border-2 border-black text-center">
+				<span><Icon name="check" size="2xl" /></span>
+				<span>24,1 kg</span>
+			</div>
+			<div class="flex h-20 w-20 flex-col justify-evenly border-2 border-black text-center">
+				<span><Icon name="check" size="2xl" /></span>
+				<span>24,1 kg</span>
+			</div>
+			<div class="flex h-20 w-20 flex-col justify-evenly border-2 border-black text-center">
+				<span class="text-2xl">3</span>
+			</div>
+			<div class="flex h-20 w-20 flex-col justify-evenly border-2 border-black text-center">
+				<span class="text-2xl">4</span>
+			</div>
+			<div class="flex h-20 w-20 flex-col justify-evenly border-2 border-black text-center">
+				<span><Icon name="plus" size="2xl" /></span>
+			</div>
+		</div>
 	</main>
 </div>
