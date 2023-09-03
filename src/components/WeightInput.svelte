6229754633710a76a2abcffd19649104e77786ee
Yan Siegers <hello@yansiegers.nl>
2023-09-03 11:18:37 +0200

    Add way to change current weight

--- src/components/WeightInput.svelte
+++ src/components/WeightInput.svelte
@@ -10,15 +10,29 @@
 		name: string;
 		weight: number;
 	}
+
+	let currentWeight = exercise.weight;
+	const step = 0.1;
+
+	function addStepToCurrentWeight() {
+		currentWeight += step;
+	}
+	function subtractStepToCurrentWeight() {
+		currentWeight -= step;
+	}
 </script>
 
 <p class="mb-3 text-center">Current weight</p>
 <div class=" flex items-center justify-center gap-4">
-	<div class="border-2 border-black px-5 py-2"><Icon name="minus" size="xl" /></div>
+	<button class="border-2 border-black px-5 py-2" on:click={subtractStepToCurrentWeight}
+		><Icon name="minus" size="xl" /></button
+	>
 	<input
 		class="w-32 border-2 border-black px-4 py-3 text-center text-4xl"
 		type="text"
-		value={exercise.weight.toLocaleString()}
+		value={currentWeight.toLocaleString()}
 	/>
-	<div class="border-2 border-black px-5 py-2"><Icon name="plus" size="xl" /></div>
+	<button class="border-2 border-black px-5 py-2" on:click={addStepToCurrentWeight}
+		><Icon name="plus" size="xl" /></button
+	>
 </div>
