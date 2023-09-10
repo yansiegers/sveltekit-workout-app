ef5db16da5a3bfc0b7db603381163adcf9f860d8
Yan Siegers <hello@yansiegers.nl>
2023-09-10 12:29:53 +0200

    Copy routine page

---
+++ src/routes/workout-routines/0/finish/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import FloatingButton from '../../../../components/FloatingButton.svelte';
+	import Header from '../../../../components/Header.svelte';
+	import OrderableExercisesList from '../../../../components/OrderableExercisesList.svelte';
+
+	interface WorkoutExercise {
+		type: 'workoutExercise';
+		sets: number;
+		reps: number;
+		name: string;
+		weight: number;
+	}
+	interface WarmingUpExercise {
+		type: 'warmingUpExercise';
+		distance: number;
+		name: string;
+	}
+
+	let workoutRoutine = { id: 0, name: 'Chest & back' };
+	let subHeading = 'Summary Friday 4th of August';
+
+	let workoutExercises: WorkoutExercise[] = [
+		{ type: 'workoutExercise', sets: 4, reps: 10, name: 'Shoulder Press', weight: 24.1 },
+		{ type: 'workoutExercise', sets: 4, reps: 10, name: 'Pec Fly', weight: 59 },
+		{ type: 'workoutExercise', sets: 4, reps: 10, name: 'Reverse Lat Pull', weight: 41.3 },
+		{ type: 'workoutExercise', sets: 4, reps: 10, name: 'Bench Press', weight: 40 },
+		{ type: 'workoutExercise', sets: 4, reps: 10, name: 'Incline Press', weight: 45.4 },
+		{ type: 'workoutExercise', sets: 4, reps: 10, name: 'Supine Press', weight: 55.4 },
+		{ type: 'workoutExercise', sets: 4, reps: 10, name: 'Decline Press', weight: 40 },
+		{ type: 'workoutExercise', sets: 4, reps: 10, name: 'Lat Pulls', weight: 40 }
+	];
+	let warmingUpExercises: WarmingUpExercise[] = [
+		{ type: 'warmingUpExercise', distance: 500, name: 'Rowing' }
+	];
+</script>
+
+<div class="flex h-screen flex-col">
+	<main class=" h-full overflow-y-scroll p-8">
+		<Header heading={workoutRoutine.name} {subHeading} exit={true} href="/" />
+
+		<div class="mb-24">
+			<OrderableExercisesList title="Warming up" exercises={warmingUpExercises} />
+			<OrderableExercisesList title="Workout" exercises={workoutExercises} />
+		</div>
+
+		<FloatingButton text="Finish!" href="/workout-routines/0/exercises" />
+	</main>
+</div>
